3885d1e4d7afdd2280fcf3abedbea7c1f60d1453
marepstein <marissaepstein@outlook.com>
2019-11-05 17:21:19 +0000

    day 3

--- scripts/app.js
+++ scripts/app.js
@@ -8,89 +8,92 @@ function spaceInvaderGame() {
   const grid = document.querySelector('.grid')
   const cells = []
   let player = 389
-  const aliens = [44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+  let aliens = [44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
     64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
     84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95]
   const topRow = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+  const bottomRow = [379, 380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399]
   const gridSize = width ** 2
-  const playerBullet = []
-  let alienBomb = []    // you need an empty array for anything that moves on the grid 
   let direction = 1
-  let lives = 0
-  let score = 0 
+  let lives = 3
+  let score = 0
   let fireBulletId
   let bulletPosition
+  const startButton = document.querySelector('button')
 
   for (let i = 0; i < width ** 2; i++) {
     const cell = document.createElement('div') // this is creating the square cells in the grid 
     grid.appendChild(cell)  // you need to append the cells as children to the grid so the div's are added to the grid array! 
     cells.push(cell)        // push the cell div into the cells array - this creates an index for each cell/div - this allows us to give our player a position as they have a number 
-    playerBullet.push(cell)
-    alienBomb.push(cell)
   }
 
-  cells[player].classList.add('player') // player on canvas 
-
-  aliens.forEach((e) => {
-    cells[e].classList.add('alien')    /// aliens on canvas
-  })
-	
-
-  function removeAliens() {
-    aliens.forEach(alien => {
-      cells[alien].classList.remove('alien')
+  function playGame() {
+    cells[player].classList.add('player') // player on canvas 
+    aliens.forEach((e) => {
+      cells[e].classList.add('alien')    /// aliens on canvas
     })
+    moveAliens()
+    randomBomb()
   }
+  startButton.addEventListener('click', playGame)
 
-  function moveAliensRight() {
-    for (var i = 0; i < aliens.length; i++) {
-      aliens[i] += 1
-      if (aliens[aliens.length - 1] % width === width - 1) {
-        console.log('hello')	
-        // clearInterval(intervalId)
+
+  function moveAliens() {
+    setInterval(() => {
+      aliens.forEach(alien => {
+        cells[alien].classList.remove('alien')
+      })
+      for (var i = 0; i < aliens.length; i++) {
+        aliens[i] += 1
       }
-    }
-  }
+      aliens.forEach(alien => {
+        cells[alien].classList.add('alien')
+      })
 
-  function addAliens() {
-    aliens.forEach(alien => {
-      cells[alien].classList.add('alien')
-    })
+      const rightWall = aliens[aliens.length - 1] % width === width - 1
+      const leftWall = aliens[0] % width
+
+      // if (rightWall === true ) {
+      //   moveDown() 
+      // }
+    }, 500)
   }
 
   function moveDown() {
-    for (var i = 0; i < aliens.length; i++) {
-      aliens[i] += width 
-    }
+    aliens.forEach(alien => {
+      cells[alien].classList.remove('alien')
+    })
+    aliens = aliens.map(alien => alien += width)
+    aliens.forEach(alien => {
+      cells[alien].classList.add('alien')
+    })
+    aliens = aliens.map(alien => alien -= 1)
   }
 
-  function downAndLeft() {
-    for (var i = 0; i < aliens.length; i++) {
-      aliens[i] -= 1
-    }
-  }
-
-
-  function test() {
-    removeAliens()
-    moveAliensRight()
-    // moveDown()
-    addAliens()
-  } 
-	
-  setInterval(test, 500) 
+  // function downAndLeft() {
+  //   for (var i = 0; i < aliens.length; i++) {
+  //     aliens[i] -= 1
+  //   }
+  // }
 
   const bulletSuperArray = []
-  let bulletsFired = 0 
+  let bulletsFired = 0
   let newBullet = player - width
-	
-  function shotBullet() {		
+
+
+  function shotBullet() {
+    // this creates a bullet array containing arrays - to allow for effectively a loop 
     const bulletArray = bulletSuperArray[bulletsFired]
     const shotInterval = setInterval(() => {
+      // at the first position of the bulletArray remove the bullet when shot
       cells[bulletArray[0]].classList.remove('bullet')
-      newBullet = bulletArray[0] - width 
+      // create new bullet, which is the initial position - width --> this creates the movement upwards 
+      newBullet = bulletArray[0] - width
+      // adds the bullet into the cells --> again contributing to the movement upwards
       cells[newBullet].classList.add('bullet')
+      // take the last eleement from the bulletArray as it's been shot --> when logged, it logs empty arrays // THINK OF THIS AS EQUIVALENT TO REMOVING THE CLASS IMAGE
       bulletArray.pop()
+      // push the new bullet into the bullet Array --> this logs every position the bullet enters up the grid // EQUIVALENT OF ADDING CLASS IMAGE 
       bulletArray.push(newBullet)
       // checks if bullet has hit an alien to remove both alien and bullet 
       for (let i = 0; i < aliens.length; i++) {
@@ -98,14 +101,16 @@ function spaceInvaderGame() {
           console.log('hit')
           clearInterval(shotInterval)
           cells[newBullet].classList.remove('alien', 'bullet')
-          aliens.splice(i, 1) 
+          // start from alien position and remove one 
+          aliens.splice(i, 1)
           // add function to +score 
         }
       }
-		}, 50)
-		
+    }, 50)
+
+
     setTimeout(() => {
-      clearInterval(shotInterval) 
+      clearInterval(shotInterval)
       setTimeout(() => {
         topRow.forEach((i) => {
           if (cells[i].className === 'bullet') {
@@ -115,6 +120,55 @@ function spaceInvaderGame() {
       }, 30)
     }, 925)
   }
+
+  const bombSuperArray = []
+  let bombsFired = 0
+  let newBomb
+
+  function dropBombs() {
+    const bombArray = bombSuperArray[bombsFired]
+    const bombInterval = setInterval(() => {
+      cells[bombArray[0]].classList.remove('bomb')
+      newBomb = bombArray[0] + width
+      cells[newBomb].classList.add('bomb')
+      bombArray.pop()
+      bombArray.push(newBomb)
+      if (cells[bombArray].classList.contains('player')) {
+        console.log('hello')
+      } 
+      console.log(newBomb)
+      if (newBomb >= 379) {
+        console.log(newBomb)
+        clearInterval(bombInterval)
+        setTimeout(() => {
+          cells[bombArray].classList.remove('bomb')
+        }, 60)
+      }
+    }, 100)
+  }
+	
+  let randomBombs 
+  function randomBomb() {
+    setInterval(() => {
+      randomBombs = aliens[Math.floor(Math.random() * aliens.length)] 
+      defineBombs()	
+    }, 500)
+  }
+	
+  function defineBombs() {
+    cells[randomBombs + width].classList.add('bomb')
+    bombSuperArray.push([randomBombs + width])
+    dropBombs()
+    bombsFired += 1
+  }
+	
+  // function loseLife() {
+  //   if (cells[player].classList.contains('bomb')) {
+  //     console.log('hello')
+  //   }
+  // }
+
+  // loseLife()
 
   document.addEventListener('keyup', (e) => {
     switch (e.key) {
@@ -137,15 +191,20 @@ function spaceInvaderGame() {
         break
       }
       case ' ': {
+        e.preventDefault()
+        // set initial bullet position
         bulletPosition = player - width
+        //push the bullet position into the bullet array - bulletPosition = the number of the square above the player
         bulletSuperArray.push([bulletPosition])
+        // shoot the bullet
         shotBullet()
+        // this basically allows for the firing of multiple bullets at once, it creates a new loop of arrays for new position
         bulletsFired += 1
       }
         break
     }
+
   })
-	
 
 
   // setup alien position - use array square id for setting different values 
